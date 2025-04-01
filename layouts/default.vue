86e7bb4c251a95f6b654cebbd279241e51e9496a
hosseiny <mohamedelhosseiny10@gmail.com>
2025-04-01 19:09:17 +0200

    feat: add default layout with page title for Quranic copier

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+  // add a title to the page
+  useHead({
+    title: "الناسخ القرآني - انسخ أي آية بسهولة",
+  });
+</script>
