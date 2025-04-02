76277b13059637a667cfe58ea1e512513be6952b
hosseiny <mohamedelhosseiny10@gmail.com>
2025-04-02 14:19:06 +0200

    feat: use hotjar

--- app.vue
+++ app.vue
@@ -7,4 +7,12 @@
 
 <script setup lang="ts">
   import { Analytics } from "@vercel/analytics/nuxt";
+
+  // In you app.vue file for example when you user has given consent:
+
+  const { initialize } = useHotjar();
+
+  function onConsent() {
+    initialize();
+  }
 </script>
